1c60b738c2340e874e02666f2c988726a6011f29
colmfah <colmdfahey@gmail.com>
2021-03-03 07:40:02 +0000

    ad nav bar

---
+++ static/js/script.js
@@ -0,0 +1,6 @@
+console.log('connected')
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+});
