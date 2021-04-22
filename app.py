ba5299d3fefea8f7600f7ea485b45f83f4046d73
colmfah <colmdfahey@gmail.com>
2021-04-22 07:14:24 +0000

    remove typo

--- app.py
+++ app.py
@@ -341,4 +341,5 @@ def handle_exception(exception):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=False)s
+            debug=False)
+s
